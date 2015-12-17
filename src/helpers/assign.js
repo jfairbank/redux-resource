96bbbffc75d2a0b949111ab67b598455feedb329
Jeremy Fairbank <elpapapollo@gmail.com>
2015-12-16 21:13:49 -0500

    Remove unused file

--- src/helpers/assign.js
+++
@@ -1,9 +0,0 @@
-export default function assign(dest, ...sources) {
-  sources.forEach((source) => {
-    Object.keys(source).forEach((key) => {
-      dest[key] = source[key];
-    });
-  });
-
-  return dest;
-}
